c88dbf3567df415e943a8176ec06a399bca58484
lailynovita <lailynovita29@gmail.com>
2025-02-17 18:29:17 +0700

    controller

--- app/Http/Controllers/AdminSdmController.php
+++ app/Http/Controllers/AdminSdmController.php
@@ -7,8 +7,6 @@ use App\Models\History;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
-
 class AdminSdmController extends Controller
 {
     public function index(Request $request)
